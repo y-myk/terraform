e65191422737138b11703edd970990c89b193dba
My name <me@example.com>
2021-07-13 16:09:19 +0100

    modified Azure/parse-template.py

--- Azure/parse-template.py
+++ Azure/parse-template.py
@@ -1,105 +1,19 @@
-{
-    "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
-    "contentVersion": "8.1.1.0",
-    "parameters": {
-        "adminUsername": {
-            "value": "azureuser"
-        },
-        "authenticationType": {
-            "value": "password"
-        },
-        "adminPasswordOrKey": {
-            "value": ""
-        },
-        "dnsLabel": {
-            "value": ""
-        },
-        "instanceName": {
-            "value": "f5vm01"
-        },
-        "numberOfExternalIps": {
-            "value": 1
-        },
-        "instanceType": {
-            "value": "Standard_DS3_v2"
-        },
-        "imageName": {
-            "value": "AllTwoBootLocations"
-        },
-        "bigIpVersion": {
-            "value": "15.1.004000"
-        },
-        "bigIpModules": {
-            "value": "ltm:nominal"
-        },
-        "licenseKey1": {
-            "value": ""
-        },
-        "vnetName": {
-            "value": ""
-        },
-        "vnetResourceGroupName": {
-            "value": ""
-        },
-        "mgmtSubnetName": {
-            "value": ""
-        },
-        "mgmtIpAddress": {
-            "value": "DYNAMIC"
-        },
-        "externalSubnetName": {
-            "value": ""
-        },
-        "externalIpAddressRangeStart": {
-            "value": "DYNAMIC"
-        },
-        "internalSubnetName": {
-            "value": ""
-        },
-        "internalIpAddress": {
-            "value": "DYNAMIC"
-        },
-        "avSetChoice": {
-            "value": "CREATE_NEW"
-        },
-        "zoneChoice": {
-            "value": "1"
-        },
-        "provisionPublicIP": {
-            "value": "Yes"
-        },
-        "declarationUrl": {
-            "value": "NOT_SPECIFIED"
-        },
-        "ntpServer": {
-            "value": "0.pool.ntp.org"
-        },
-        "timeZone": {
-            "value": "UTC"
-        },
-        "customImageUrn": {
-            "value": "OPTIONAL"
-        },
-        "customImage": {
-            "value": "OPTIONAL"
-        },
-        "restrictedSrcAddress": {
-            "value": ""
-        },
-        "tagValues": {
-            "value": {
-                "application": "APP",
-                "cost": "COST",
-                "environment": "ENV",
-                "group": "GROUP",
-                "owner": "OWNER"
-            }
-        },
-        "allowUsageAnalytics": {
-            "value": "Yes"
-        },
-        "allowPhoneHome": {
-            "value": "Yes"
-        }
-    }
-}
+#!/usr/bin/env python3
+#
+# This script pulls the parameters from F5 ARM templates for use in terraform files
+#
+
+import json
+
+# Open the f5 CFT or ARM template file
+with open('./azuredeploy.parameters.json') as json_file:
+    # Import the file as a JSON object
+    data = json.load(json_file)
+    # Loop through each parameter (p) in the parameters section
+    paramlist = []
+    for p in data['parameters']:
+            value = str(data['parameters'][p]['value'])
+            paramlist.append(p + ':  "' + value + '"')
+    paramlist = (sorted(paramlist))
+    for p in paramlist:
+        print(p)
