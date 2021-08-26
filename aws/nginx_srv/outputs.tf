d46f9d3ecd0436f3657ea242384b3d2ea2028ed6
ymyk <7059538+y-myk@users.noreply.github.com>
2021-08-26 14:05:14 +0100

    Delete aws/nginx_srv directory

--- aws/nginx_srv/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_public_ip" {
-  value = "${aws_instance.ubuntu18-04-LTS.public_ip}"
-}
--- aws/nginx_srv/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "key_name" {
-  default = "ubuntu-18-04"
-}
-variable "ami_id" {
-  default = "ami-0a246166b9abf0c0a"
-}
-variable "region" {
-  default = "us-west-1"
-}
